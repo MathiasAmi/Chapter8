3f37d097336ca39f71fca697225c97e9bb9b1c22
MathiasAmi <42993917+MathiasAmi@users.noreply.github.com>
2018-10-24 15:09:20 +0200

    Update Chapter8.java
    Added the exercise' description

--- src/Chapter8.java
+++ src/Chapter8.java
@@ -4,6 +4,11 @@ public class Chapter8{
 
     public static void main(String[] args) {
 
+           //Opgave 8.1
+        // Write a method that returns the sum of all the elements in a specified column in a matrix using the following
+        //  header: public static double sumColumn(double[][] m, int columnIndex)
+        // Write a test program that reads a 3-by-4 matrix and displays the sum of each column.
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a 3-by-4 matrix row by row: ");
         double[][] number = new double[3][4];
